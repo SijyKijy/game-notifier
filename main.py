41eddfa84f301840c3254b3f4648a3c99e928620
Max <SijyKijy@users.noreply.github.com>
2024-07-28 04:30:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -129,7 +129,8 @@ def Notify(game):
     response = requests.post(f'https://discord.com/api/webhooks/{WEBHOOK_PATH}', json = webhookContent)
     if response.status_code != 200:
         print(f'[Notify] Req: {webhookContent}')
-        print(f'[Notify] Resp: {response.text}')
+        print(f'[Notify] [{response.status_code}] Resp: {response.text}')
+        print(f'[Notify] Resp2: {response}')
         raise Exception('Error when notify game')
 
     print('[Notify] Done')
